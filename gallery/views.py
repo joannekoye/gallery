538fb492f4d93b744170131f5e33040c34ebef63
Joan Nekoye <joannekoye@gmail.com>
2020-05-23 20:00:38 +0300

    create views, urls, html files and add bootstrap

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    '''
+    view function to display landing page
+    '''
+
+    return render(request, 'index.html')
