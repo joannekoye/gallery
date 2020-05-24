aad5b6f0f97708de1f620af9e94319384ac739de
Joan Nekoye <joannekoye@gmail.com>
2020-05-25 00:37:57 +0300

    create static route for uploaded files

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('locations/sorted/', views.sort_by_locations, name='sort_by_locations'),
     path('image/<image_id>/', views.single_image, name= 'single_image')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
