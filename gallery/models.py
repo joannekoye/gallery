ae71db293a312e58dfa6ee7ef08dd2ecef024977
Joan Nekoye <joannekoye@gmail.com>
2020-05-24 14:01:52 +0300

    create models and run migration 0001

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=30)
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+class Image(models.Model):
+    img_path = models.ImageField(upload_to= 'images/')
+    img_name = models.CharField(max_length=30)
+    img_desc = models.TextField(blank=True)
+    img_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    img_category = models.ForeignKey(Category, on_delete=models.CASCADE)
