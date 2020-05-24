70237a82fe32f768f3be52818273d493a314196a
Joan Nekoye <joannekoye@gmail.com>
2020-05-24 22:40:49 +0300

    create index view

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Location, Image, Category
+from django.http import Http404
 
 def index(request):
     '''
     view function to display landing page
     '''
+    images = Image.objects.all()
 
-    return render(request, 'index.html', )
+    return render(request, 'index.html', {'images': images})
