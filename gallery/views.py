da89a24de4aafc58955f6b2cd8f675f025d6c5de
Joan Nekoye <joannekoye@gmail.com>
2020-05-24 22:48:37 +0300

    set up search url pattern and view

--- gallery/views.py
+++ gallery/views.py
@@ -9,3 +9,19 @@ def index(request):
     images = Image.objects.all()
 
     return render(request, 'index.html', {'images': images})
+
+def search_page(request):
+    '''
+    view function to open search page and display searched images
+    '''
+
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        images = Image.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message':message, 'images': images})
+
+    else:
+        message = 'Please enter a term to be sought for'
+        return render(request, 'search.html', {'message': message})
